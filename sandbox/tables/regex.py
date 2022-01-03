d7f5ee594af1f06ac710690a63c88d7a15317c2a
davemolk <davemolk@gmail.com>
2022-01-03 06:47:39 -0700

    table with regex and regex_soup

---
+++ sandbox/tables/regex.py
@@ -0,0 +1,20 @@
+from bs4 import BeautifulSoup
+
+import re
+
+
+with open('info.html') as html_file:
+    soup = BeautifulSoup(html_file, 'lxml')
+
+
+# just regex
+email = re.findall(r'([\d\w\-\.]+@[\d\w\-\.]+\.\w+)', soup.text)
+phone_numbers = re.findall(r'1-[\d]{3}-[\d]{3}-[\d]{4}', soup.text)
+
+
+# regex and soup
+soup_email = soup.find_all('td', string=re.compile(r'([\d\w\-\.]+@[\d\w\-\.]+\.\w+)'))
+cleaned_email = [email.text for email in soup_email]
+
+soup_phone = soup.find_all('td', string=re.compile(r'1-[\d]{3}-[\d]{3}-[\d]{4}'))
+cleaned_phone = [phone.text for phone in soup_phone]
